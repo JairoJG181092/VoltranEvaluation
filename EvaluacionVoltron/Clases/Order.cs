8748e5987e7d159bb92d3af9f0d6fc65f24fdf70
JairoJG181092 <99001740+JairoJG181092@users.noreply.github.com>
2023-10-10 17:16:49 -0600

    Primera Carga Proyecto
    Se realiza la primera carga de la aplicación en su totalidad

---
+++ EvaluacionVoltron/Clases/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionVoltron.Clases
+{
+    internal class Order
+    {
+        private double standardLength = 2.5;
+        private double[] availableHeights = { 1.03, 1.53, 2.03 };
+        private Dictionary<double, int> fixatorsPerHeight = new Dictionary<double, int>
+        {
+            { 1.03, 3 },
+            { 1.53, 4 },
+            { 2.03, 6 }
+        };
+        private List<string> availableColors = new List<string> { "sin pintura", "blanca", "negra", "verde" };
+
+        
+
+        public string GetOrderSummary(double requestedLength, double height, string color)
+        {
+            double soldLength = Math.Max(requestedLength, standardLength);
+            double fencestoSell = Math.Ceiling(requestedLength / standardLength);
+            double lengthDifference = (fencestoSell * standardLength) - requestedLength;
+            int fixators = fixatorsPerHeight[height];
+            int firstFence = 2;
+            double restFences = fencestoSell - 1;
+
+            double totalPostSell = firstFence + restFences;
+
+            string summary = "";
+
+            summary = $"Altura: {height}m | Color: {color} | Largo vendido: {soldLength}m | Cercas vendidas {fencestoSell} | Fijadores por poste {fixators} | Postes a vender {totalPostSell} ";
+
+            if (lengthDifference > 0)
+            {
+                summary += $" | Diferencia de largo: {lengthDifference} m";
+            }
+           
+            
+
+            return summary;
+        }
+    }
+}
---
+++ EvaluacionVoltron/Datos/AccessData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EvaluacionVoltron.Datos
+{
+    internal class AccessData
+    {
+        string conn = ConfigurationSettings.AppSettings["conexion"];
+        public string insertapedido(double largo, double alto, string color)
+        {
+            var resp = "";
+
+
+            using (SqlConnection connx = new SqlConnection(conn))
+            {
+                using (SqlCommand command = new SqlCommand("InsertarEnTabla", connx))
+                {
+                    try
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Parámetros del procedimiento almacenado
+                        command.Parameters.Add("@p_largo", SqlDbType.VarChar, 255).Value = largo.ToString();
+                        command.Parameters.Add("@p_alto", SqlDbType.VarChar, 255).Value = alto.ToString();
+                        command.Parameters.Add("@p_color", SqlDbType.VarChar, 255).Value = color;
+                        //command.Parameters.Add("@p_confirmacion_pedido", SqlDbType.Bit).Value = confirmacion;
+                        command.Parameters.Add("@p_fecha_confirmacion", SqlDbType.DateTime).Value = DateTime.Now;
+
+                        connx.Open();
+                        command.ExecuteNonQuery();
+                        resp = "1";
+                    }
+                    catch (Exception ex)
+                    {
+                        resp = "Ocurrio un error al insertar en la BD: " + ex.Message;
+                    }
+                }
+            }
+
+            return resp;
+        }
+
+
+
+        public List<string> ConsultarDatos()
+        {
+            List<string> datos = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                using (SqlCommand command = new SqlCommand("ConsultarDatos", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string itemText = $"{reader["largo"]} - {reader["alto"]} - {reader["color"]} - {reader["fecha_confirmacion"]}";
+                            datos.Add(itemText);
+                        }
+                    }
+                }
+            }
+
+            return datos;
+        }
+    }
+
+}
